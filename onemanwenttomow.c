72a5565ea701b536013e80d2084f4c4a2b01c4a9
Joel-Ch <joelchiappetti@gmail.com>
2022-10-27 00:43:18 +0100

    really coding at 1am for fun

---
+++ onemanwenttomow.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int Line3(int count)
+{
+    for (count; count > 1; count--)
+    {
+        printf("%i men, ", count);
+    }
+
+    printf("1 man and his dog spot");
+    
+    return 0;
+}
+
+int main(void)
+{
+    int BigCount;
+    printf("enter your start value\n");
+    scanf("%i", &BigCount);
+
+    for ( BigCount; BigCount > 1; BigCount--)
+    {
+        printf("%i men went to mow\nwent to mow a meadow\n", BigCount);
+        Line3(BigCount);
+        printf("\nwent to mow a meadow\n\n");
+    }
+
+    printf("%i man went to mow\nwent to mow a meadow\n", BigCount);
+    Line3(BigCount);
+    printf("\nwent to mow a meadow\n\n");
+    
+    
+}
+
