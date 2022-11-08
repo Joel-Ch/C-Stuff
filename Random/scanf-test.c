eda887304c38cfa66388beba7e9cf3fb76d12317
Joel-Ch <joelchiappetti@gmail.com>
2022-11-08 17:39:21 +0000

    scanf testing

---
+++ Random/scanf-test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    int check;
+    float value;
+
+    printf("help");
+    check = scanf("%f", &value);
+
+    printf("%i, %i", check, value);
+}
