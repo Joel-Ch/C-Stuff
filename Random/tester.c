7fc8eb50c926e9107d04fc60d413038f0ffd8f97
Joel-Ch <joelchiappetti@gmail.com>
2022-11-11 09:27:37 +0000

    no cheating here!

---
+++ Random/tester.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 7, c = 3;
+    int i = 0;
+
+    // if (a = b)
+    // {
+    //     printf("%d ", (a + b))
+    // }
+    // else
+    // {
+    //     printf("%d ", (a - b));
+    // }
+
+    // printf("%d", c)
+
+    for (i = 1; i <= 6; i++)
+        ;
+    printf("Current value is %d ", i);
+}
