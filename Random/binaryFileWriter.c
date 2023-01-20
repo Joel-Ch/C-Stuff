9816d54f175426e17de9268f88759aed95a40dde
Joel-Ch <joelchiappetti@gmail.com>
2023-01-20 15:28:24 +0000

    nope

---
+++ Random/binaryFileWriter.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Invalid usage, correct use: %s <FILENAME> <string to write>", argv[0]);
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "rb");
+    if (fp == NULL)
+    {
+        printf("error allocating memory, please retry");
+        return 2;
+    }
+    fwrite(argv[2], sizeof(char), strlen(argv[2]), fp);
+    printf("written, exiting");
+
+    fclose(fp);
+
+    return 0;
+}
---
+++ Random/tempCodeRunnerFile.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Invalid usage, correct use: %s <FILENAME> <number of character to be read", argv[0]);
+        return 1;
+    }
+    char *p = calloc(argv[2], sizeof(char));
+    FILE *fp = fopen(argv[1], "rb");
+    if (fp == NULL)
+    {
+        printf("error allocating memory, please retry");
+        return 2;
+    }
+    fread(p, sizeof(char), argv[2], fp);
+    for (size_t i = 0; i < argv[2]; i++)
+    {
+        printf("Number %2i: %c", i, p[i]);
+    }
+    
+    fclose(fp);
+
+    free(p);
+
+    return 0;
+}
